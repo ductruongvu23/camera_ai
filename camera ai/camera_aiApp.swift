509d2780ff168eadb5168ae2af66147be263c8c6
ductruongvu23 <ductruongvu23@gmail.com>
2026-09-15 10:54:29 +0700

    Initial Commit

---
+++ camera ai/camera_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  camera_aiApp.swift
+//  camera ai
+//
+//  Created by vdt on 15/9/26.
+//
+
+import SwiftUI
+
+@main
+struct camera_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
